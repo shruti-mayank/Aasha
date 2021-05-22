4a51c4fef7620ff2512a53ef759b980b2cb0ebac
Shruti Mayank <shrutimayank1@gmail.com>
2021-05-22 18:53:57 +0545

    Investee Login Updated

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/newBlog', (req, res)=>{
     res.render('newBlog', {active:"newBlog"});
 });
 
-app.post('/blogPage', (req, res)=>{
+app.get('/blogPage', (req, res)=>{
     res.render('blogPage', {active:"blogPage"});
 });
 
